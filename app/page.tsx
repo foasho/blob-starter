c1840032f0c78a3c91c96f3f25df61e5204f4d2b
foasho <foa.sho0111@outlook.jp>
2024-01-10 20:40:45 +0900

    update

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,9 @@ import { useState, useRef } from 'react';
 export default function AvatarUploadPage() {
   const inputFileRef = useRef<HTMLInputElement>(null);
   const [blob, setBlob] = useState<PutBlobResult | null>(null);
+  const [loading, setLoading] = useState(false);
   return (
-    <>
+    <div className='p-4'>
       <h1>Upload Your More Than 4.5MB File</h1>
  
       <form
@@ -21,22 +22,26 @@ export default function AvatarUploadPage() {
  
           const file = inputFileRef.current.files[0];
  
+          setLoading(true);
           const newBlob = await upload(file.name, file, {
             access: 'public',
             handleUploadUrl: '/api/upload',
           });
  
           setBlob(newBlob);
+          setLoading(false);
         }}
       >
-        <input name="file" ref={inputFileRef} type="file" required />
-        <button type="submit">Upload</button>
+        <input name="file" ref={inputFileRef} type="file" required disabled={loading} />
+        <button type="submit" disabled={loading} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          アップロード
+        </button>
       </form>
       {blob && (
         <div>
           Blob url: <a href={blob.url}>{blob.url}</a>
         </div>
       )}
-    </>
+    </div>
   );
 }
